29a385a64e309db48ed3d09471e2562af18249e6
Pedro Tabacof <pedrotabacof@Pedros-MacBook-Pro.local>
2023-06-02 16:04:52 +0100

    serverless updates

--- posts/serverless_model_deployment/flask_endpoint.py
+++ posts/serverless_model_deployment/flask_endpoint.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, jsonify
-import pickle
-from sklearn.exceptions import NotFittedError
+import cloudpickle
 
 app = Flask(__name__)
 
 # Load your model at startup
 try:
     with open('model.pickle', 'rb') as f:
-        model = pickle.load(f)
+        model = cloudpickle.load(f)
 except (OSError, IOError) as e:
     print(f"Error: {e}")
     model = None
@@ -21,5 +20,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# Run with `gunicorn -w 8 flask_endpoint:app` on the command line
 if __name__ == "__main__":
     app.run(debug=False)
+    
